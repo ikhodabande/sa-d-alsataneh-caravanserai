bc68e9f1fb8617aee06e76dc7a1ec1c66bb70fbd
Amirmohammad <amii_mhmd@yahoo.com>
2024-03-11 20:02:27 +0330

    adding navbar fa words

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,13 @@ function Navbar() {
     <div className='flex justify-between p-2' name='navbar'>
 
     <div>
-    <p className='text-4xl inline font-bold text-red-600 py-2 pl-2 cursor-pointer'>SA-D ALSALTANEH</p>
-    <p className='inline text-2xl font-bold cursor-pointer'>Carvanseri</p>
+    <p className='text-4xl inline font-bold text-red-600 py-2 pl-2 cursor-pointer'> سعد السلطنه</p>
+    <p className='inline text-2xl font-bold cursor-pointer'>کاروانسرا </p>
     </div>
 
-    <div>
-    <button className='pr-2 cursor-pointer'>Sign In</button>
-    <button className='bg-red-600 rounded py-2 px-4 text-white cursor-pointer '>Sign Up</button>
+    <div className='flex items-center justify-center'>
+    <button className='pr-2 cursor-pointer mx-2'>ثبت نام</button>
+    <button className='bg-red-600 rounded py-1.5 px-5 pb-2.5  text-white cursor-pointer hover:bg-[#b50303] '>ورود</button>
     </div>
 
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+    <div className="">
+      <img src="" alt="" />
+    </div>
+    
+    </>
+  )
+}
+
+export default Home
